9509cf52428d031fb7eaebc5d1e4bec1cca3bcfd
Tea-ops-cloud <tearushi123@gmail.com>
2025-03-13 21:11:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+
+    function kontrolloPergjigjen(eshteSakte) {
+        let rezultati = document.getElementById("rezultati");
+        if (eshteSakte) {
+            rezultati.innerHTML = "Përgjigja e saktë është Jodi!";
+            rezultati.style.color = "green";
+        } else {
+            rezultati.innerHTML = "Gabim! Përgjigja e saktë është Jodi.";
+            rezultati.style.color = "red";
+        }
+    }
+
+function kontrolloPergjigjen1(eshteSakte) {
+        let rezultati1 = document.getElementById("rezultati1");
+        if (eshteSakte) {
+            rezultati1.innerHTML = "Përgjigja e saktë është Kalciumi!";
+            rezultati1.style.color = "green";
+        } else {
+            rezultati1.innerHTML = "Gabim! Përgjigja e saktë është Kalciumi.";
+            rezultati1.style.color = "red";
+        }
+    }
+
+function kontrolloPergjigjen2(eshteSakte) {
+        let rezultati2 = document.getElementById("rezultati2");
+        if (eshteSakte) {
+            rezultati2.innerHTML = "Përgjigja e saktë është Hekuri!";
+            rezultati2.style.color = "green";
+        } else {
+            rezultati2.innerHTML = "Gabim! Përgjigja e saktë është Hekuri.";
+            rezultati2.style.color = "red";
+        }
+    }
+
+function kontrolloPergjigjen3(eshteSakte) {
+        let rezultati3 = document.getElementById("rezultati3");
+        if (eshteSakte) {
+            rezultati3.innerHTML = "Përgjigja e saktë është Azoti!";
+            rezultati3.style.color = "green";
+        } else {
+            rezultati3.innerHTML = "Gabim! Përgjigja e saktë është Azoti.";
+            rezultati3.style.color = "red";
+        }
+    }
+
+
+
+
+  
+  
